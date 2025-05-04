edea6bf06c952d10440c83799655ae8cf0add20f
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-05-04 10:26:50 -0300

    Apagado arquivos referentes ao trab1

--- teste/ComparadorMatricula.java
+++
@@ -1,9 +0,0 @@
-package teste;
-
-import java.util.Comparator;
-
-public class ComparadorMatricula implements Comparator<Aluno> {
-    public int compare(Aluno a1, Aluno a2) {
-        return Integer.compare(a1.matricula, a2.matricula);
-    }
-}
