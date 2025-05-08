30ba940c982c4eef5385f131bea9d3e3b37d99ad
Caio Chiabai <caiochiabaioliveira@gmail.com>
2025-05-07 22:05:45 -0300

    Criando livros ao inciar programa

--- app/AppLivro.java
+++ app/AppLivro.java
@@ -11,10 +11,34 @@ public class AppLivro {
         IArvoreBinaria<Livro> arvorePorNome = new ArvoreBinaria<>(new ComparadorLivroPorNome());
         IArvoreBinaria<Livro> arvorePorData = new ArvoreBinaria<>(new ComparadorLivroPorData());
 
+        Livro livro1 = new Livro("A Guerra dos Tronos", "George R. R. Martin",
+                "Excelente começo para uma série épica.", "10/3/2020", 9);
+        Livro livro2 = new Livro("O Hobbit", "J. R. R. Tolkien",
+                "Uma jornada leve e mágica.", "22/5/2021", 8);
+        Livro livro3 = new Livro("Dom Quixote", "Miguel de Cervantes",
+                "Um clássico com muito humor e crítica.", "5/11/2022", 7);
+        Livro livro4 = new Livro("1984", "George Orwell",
+                "Assustadoramente atual.", "14/8/2020", 10);
+        Livro livro5 = new Livro("A Revolução dos Bichos", "George Orwell",
+                "Fábula crítica e direta.", "30/1/2024", 9);
+
+        // Adicionando nas árvores
+        arvorePorNome.adicionar(livro1);
+        arvorePorNome.adicionar(livro2);
+        arvorePorNome.adicionar(livro3);
+        arvorePorNome.adicionar(livro4);
+        arvorePorNome.adicionar(livro5);
+
+        arvorePorData.adicionar(livro1);
+        arvorePorData.adicionar(livro2);
+        arvorePorData.adicionar(livro3);
+        arvorePorData.adicionar(livro4);
+        arvorePorData.adicionar(livro5);
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Escolha a opção:");
+            System.out.println("Escolha a opcao:");
             System.out.println("1. Adicionar livro");
             System.out.println("2. Buscar livro por nome");
             System.out.println("3. Buscar livro por data");
