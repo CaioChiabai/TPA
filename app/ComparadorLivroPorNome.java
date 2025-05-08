30ad034f73878501c166c83f6cd617a91c6236f1
Diego <87023558+diegobrangel@users.noreply.github.com>
2025-05-08 16:50:29 -0300

    Update ComparadorLivroPorNome.java

--- app/ComparadorLivroPorNome.java
+++ app/ComparadorLivroPorNome.java
@@ -1,10 +1,18 @@
 package app;
 
+import java.text.Normalizer;
 import java.util.Comparator;
 
 public class ComparadorLivroPorNome implements Comparator<Livro> {
     @Override
     public int compare(Livro l1, Livro l2) {
-        return l1.getNome().compareToIgnoreCase(l2.getNome());
+        String nome1 = normalizar(l1.getNome());
+        String nome2 = normalizar(l2.getNome());
+        return nome1.compareToIgnoreCase(nome2);
+    }
+
+    private String normalizar(String texto) {
+        if (texto == null) return "";
+        return Normalizer.normalize(texto, Normalizer.Form.NFD).replaceAll("[\\p{InCombiningDiacriticalMarks}]", "").trim();
     }
 }
